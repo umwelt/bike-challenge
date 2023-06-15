5e2db6e1a4672de5d6595c8c95ae13c1408dd128
supertramp <hugo@kode.zone>
2023-06-15 23:05:21 +0100

    Initial Commit

---
+++ BikeMap/BikeMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeMapApp.swift
+//  BikeMap
+//
+//  Created by Hugo Perez on 15/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BikeMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
